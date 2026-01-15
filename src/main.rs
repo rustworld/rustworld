c7b9223c052494bc0cf77db0da32b379fafc9800
rustworld <rustworld@informatrix.ch>
2026-01-15 21:18:48 +0100

    added axum serving static web pages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use axum::Router;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    println!("Start Axum Web Server");
+    // build our application with a route
+    let app = app();
+
+    // run our app with hyper, listening globally on port 8080
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn app () -> Router {
+    let static_files = ServeDir::new("./static");
+
+    Router::new()
+        .route("/hello", get(|| async {"hello"}))
+        .fallback_service(static_files)
 }
